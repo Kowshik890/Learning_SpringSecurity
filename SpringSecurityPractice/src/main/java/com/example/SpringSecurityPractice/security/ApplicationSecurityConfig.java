0b160a08541c54452e11bc163da336c6816e76ae
Kowshik Das <kowshikdas@Kowshiks-MacBook-Pro.local>
2021-11-21 21:08:10 +0100

    Learning Spring Security - Day Three - 21.11.2021

--- SpringSecurityPractice/src/main/java/com/example/SpringSecurityPractice/security/ApplicationSecurityConfig.java
+++ SpringSecurityPractice/src/main/java/com/example/SpringSecurityPractice/security/ApplicationSecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.SpringSecurityPractice.security.ApplicationUserRole.*;
+
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -27,8 +29,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*")
-                .permitAll()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -38,13 +40,20 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     @Bean
     protected UserDetailsService userDetailsService() {
-        UserDetails annaSmith = User.builder()
+        UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles("STUDENT")  // internally - ROLE_STUDENT
+                .roles(STUDENT.name())  // internally - ROLE_STUDENT
                 .build();
+
+        UserDetails lindaUser = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ADMIN.name())  // internally - ROLE_ADMIN
+                .build();
+
         return new InMemoryUserDetailsManager(
-                annaSmith
+                annaSmithUser, lindaUser
         );
     }
 }
